20c6a56b300006cf2a2dcab6af4de6a148a42955
Mo Mesgin <mo.mesgin@gmail.com>
2018-06-20 09:51:42 -0700

    deploy test

--- server.js
+++ server.js
@@ -32,25 +32,26 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-// let token;
-app.get('/', (req, res) => {
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+let tokenSaved;
+request.post(authOptions, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
 
-      // use the access token to access the Spotify Web API
-      let token = body.access_token
-      let options = {
-        url: 'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        json: true
-      }
-      res.send(token)
-      request.get(options, function (error, response, body) {
-      })
+    // use the access token to access the Spotify Web API
+    let token = body.access_token
+    let options = {
+      url: 'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
     }
-  })
+    tokenSaved = token
+    request.get(options, function (error, response, body) {
+    })
+  }
+})
+app.get('/', (req, res) => {
+  res.send(tokenSaved)
 })
 
 app.post('/', (req, res) => {
