38133e8a2ea0307efec98f0a8b22ae228b22a7ad
Mo Mesgin <mo.mesgin@gmail.com>
2018-09-15 17:09:51 -0700

    test

--- server.js
+++ server.js
@@ -77,10 +77,10 @@ const port = process.env.PORT || 8888
 
 if (process.env.NODE_ENV === 'production') {
   // app.use(express.static('/client/build'))
-  app.use('/', express.static(path.join(__dirname, 'client/build')))
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  // })
+  app.use('/', express.static(path.join(__dirname, 'client/build/static')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 app.listen(port, () => {
