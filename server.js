e1cd2b8aef8e4f3bedc5e576a97f27641d784fd7
Mo Mesgin <mo.mesgin@gmail.com>
2018-09-15 21:38:46 -0700

    test

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const key = require('./client/src/config/keys').bandKey
 const secret = require('./client/src/config/keys').secret
 const path = require('path')
 
-app.use('/client/build', express.static(path.join(__dirname, 'client', 'build')));
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
@@ -78,7 +76,7 @@ app.post('/api', (req, res) => {
 const port = process.env.PORT || 8888
 
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static('client/build'))
+  app.use(express.static(__dirname + '/client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
