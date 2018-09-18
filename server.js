3bf817182faf80d1f7a0d3e03113f031893f8818
Mo Mesgin <mo.mesgin@gmail.com>
2018-09-17 20:16:07 -0700

    test

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ const port = process.env.PORT || 8888
 
 if (process.env.NODE_ENV === 'production') {
   app.use('/static', express.static('/client/build'));
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
   // app.get('/', (req, res) => {
   //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   // })
