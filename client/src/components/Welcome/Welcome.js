89e1cb673bf843b1cb2464c30636a2f2bdbb34ac
Mo Mesgin <mo.mesgin@gmail.com>
2018-09-19 21:52:04 -0700

    test

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Welcome(props) {
   return (
     <p>
-      Search for your favourite artists to find out when and where their next concert is. It will be shown on Google Maps and by clicking on any marker, you will see more information. You can also preview their albums and listen to 30seconds of all songs.
+      Search for artists to get information about their next concert, and you can also listen to 30 seconds of all their songs.
     </p>
   )
 }
